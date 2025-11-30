4a0307f8f9c40b5bbff74a64f5421b5d6d150ed8
alma-jaarany <almajaarany@gmail.com>
2025-11-30 15:33:36 +0200

    לוח הכפל

--- AlmaJaaranysWebsite/אודות.aspx.cs
+++ AlmaJaaranysWebsite/אודות.aspx.cs
@@ -6,9 +6,23 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class אודות : System.Web.UI.Page
-{
+{ public string str;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+    
+    str = "<table>";
+        for(int i = 0; i <= 10; i++)
+        {
+            str += "<tr>";
+       
+      for(int j = 0; j <= 10; j++) 
+        { str += "<td>";
+           str += i*j; 
+                str += "</td>";
+        }
+       
+            str += "</tr>";
+        }
+    str = "</table>";
     }
 }
