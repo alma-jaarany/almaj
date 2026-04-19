94df96a7ba526899ffa718fcad11cd7b2ce21af5
alma-jaarany <almajaarany@gmail.com>
2026-04-19 14:42:03 +0300

    כניסה session

--- AlmaJaaranysWebsite/כניסה.aspx.cs
+++ AlmaJaaranysWebsite/כניסה.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,8 +15,12 @@ public partial class כניסה : System.Web.UI.Page
         {
             string password = Request.Form["password"];
             string email = Request.Form["email"];
+
             if (email == "almajaarany@gmail.com" && password == "alma2410")
             {
+                
+                Session["manager"] = "ok";
+                Session["level"] = "מנהל";
                 Response.Redirect("מנהל.aspx");
             }
             else
@@ -26,15 +31,19 @@ public partial class כניסה : System.Web.UI.Page
                     "AND password = '" + password + "'";
 
 
-                bool userExists = MyAdoHelper.IsExist(sql);
+                //bool userExists = MyAdoHelper.IsExist(sql);
+                DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
 
-                if (!userExists)
+                if (dt.Rows.Count == 0)
                 {
+                    Session["level"] = "אורח";
                     st = "אימייל או סיסמה שגויים";
                 }
                 else
                 {
                     //st = "משתמש רשום";
+                    Session["user"] = "ok";
+                    Session["level"] = dt.Rows[0]["email"];
                     Response.Redirect("דף-הבית.aspx");
                 }
             }
