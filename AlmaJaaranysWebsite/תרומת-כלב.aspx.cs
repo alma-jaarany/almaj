00f2be3497116d976bc5ee3e968a503f0bd0f029
alma-jaarany <almajaarany@gmail.com>
2026-06-05 22:58:36 +0300

    רמת משתמש

--- AlmaJaaranysWebsite/תרומת-כלב.aspx.cs
+++ AlmaJaaranysWebsite/תרומת-כלב.aspx.cs
@@ -24,12 +24,12 @@ public partial class _Default : System.Web.UI.Page
         age = Request.Form["age"];
         bread = Request.Form["bread"];
         // החלפת כל גרש בודד בשני גרשים כדי למנוע שגיאות בשאילתת SQL
-        string dogName = name.Replace("'", "''");
-        string dogAge = age.Replace("'", "''");
-        string dogBread = bread.Replace("'", "''");
+        //string dogName = name.Replace("'", "''");
+        //string dogAge = age.Replace("'", "''");
+        //string dogBread = bread.Replace("'", "''");
         string sqlInsert =
         "INSERT INTO Dogs (dogName, dogAge, dogBread) " +
-        "VALUES (N'" + dogName + "', N'" + dogAge + "', N'" + dogBread + "')";
+        "VALUES (N'" + name + "', N'" + age + "', N'" + bread + "')";
 
         MyAdoHelper.DoQuery("MyDB.mdf", sqlInsert);
         strResult = st;
